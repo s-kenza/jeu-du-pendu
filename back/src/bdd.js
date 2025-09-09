006faac1961db985d686d671cce89cf09b5ce402
schuler <kenza.schuler@aprr.fr>
2025-09-09 14:42:29 +0200

    fix: :bug: modify env parameters to railway variables

--- back/src/bdd.js
+++ back/src/bdd.js
@@ -9,9 +9,9 @@ dotenv.config();
  */
 export const sequelize = new Sequelize({
 	dialect: MySqlDialect,
-	database: process.env.DB_NAME,
-	user: process.env.DB_USER,
-	password: process.env.DB_PASSWORD,
-	host: process.env.DB_HOST,
-	port: parseInt(process.env.DB_PORT),
+	database: process.env.MYSQLDATABASE,
+	user: process.env.MYSQLUSER,
+	password: process.env.MYSQLPASSWORD,
+	host: process.env.MYSQLHOST,
+	port: parseInt(process.env.MYSQLPORT),
 });
